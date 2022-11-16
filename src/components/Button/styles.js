6a5177db906fe7aba120979dae83452f0c9bc3a7
Pedro Santos <90013568+PedroSantoss-Dev@users.noreply.github.com>
2022-11-16 15:31:51 -0300

    Feat : :100:  add calculator

---
+++ src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid rgb(43, 36, 36) ;
+    background-color: rgb(105, 105, 105);
+    color: #FFFFFF;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+    
+    &:hover {
+        opacity: 0.6;
+    }
+`
---
+++ src/components/Input/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 60px;
+    background-color: #AAAAFF;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    font-size: 24px;
+    font-family: 'Roboto';
+    border-radius: 0.4rem;
+    
+    input {
+        border-radius: 0.4rem;
+        width: 100%;
+        height: 60px;
+        background-color: #AAAAFF;
+        border: none;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        padding: 0 10px;
+        font-size: 24px;
+        font-family: 'Roboto';
+        color: #FFFFFF;
+    }
+`
